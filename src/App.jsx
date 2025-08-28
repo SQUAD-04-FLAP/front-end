571a65569ac0d7f9188e2e908918e617faaf5391
GuilhermeD9 <sgsouza.173@gmail.com>
2025-08-28 14:48:04 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-6xl font-extrabold">Init</p>
+    </div>
+  )
+}
+
+export default App;
