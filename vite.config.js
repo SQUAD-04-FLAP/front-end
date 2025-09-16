84c580e92cc2f613b3f03880abf6c36a92e5fc44
Ryan Nardelli <ryannardelli@gmail.com>
2025-09-16 17:48:48 -0300

    enviando configuração de ambiente de dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // Todas as requisições que começarem com /api
+      '/api': {
+        target: 'http://ec2-54-226-167-245.compute-1.amazonaws.com:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // remove /api ao enviar para o backend
+      },
+    },
+  },
 })
