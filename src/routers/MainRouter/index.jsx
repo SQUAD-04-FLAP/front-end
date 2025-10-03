5389231e8423d11f2540975c86060f021cd8f834
Ryan Nardelli <ryannardelli@gmail.com>
2025-10-03 11:47:04 -0300

    fix: Enviando ajuste para build

--- src/routers/MainRouter/index.jsx
+++ src/routers/MainRouter/index.jsx
@@ -6,7 +6,7 @@ import { Calendar } from "../../pages/Calendar";
 import { Report } from "../../pages/Report";
 import BoardV2 from "../../pages/BoardV2";
 import Configuracoes from "../../pages/Configuracoes";
-import AdminEmpresas from "../../pages/Admin/Empresas";
+import AdminEmpresas from '../../pages/Admin/Empresas';
 import AdminKanban from "../../pages/Admin/Kanban";
 import PublicLayout from "../../layout/PublicLayout";
 import MainTemplate from "../../templates/MainTemplate";
@@ -15,8 +15,6 @@ import { RecoveryPassword } from "../../pages/RecoveryPassword";
 import { Register } from "../../pages/Register";
 import { useAuth } from "../../hooks/useAuth";
 import { SpinnerLoading } from "../../components/SpinnerLoading";
-
-import { SectorProvider } from "../../provider/SectorProvider";
 
 export function MainRouter() {
     const { user, loading } = useAuth();
