150ab101973bc29bf7923a6b3519f200fa23fb82
cyldorta <cyliphone56@gmail.com>
2025-10-03 20:29:26 -0300

    Atualização do router com novas telas

--- src/components/ButtonNewTask/index.jsx
+++ src/components/ButtonNewTask/index.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 export function ButtonNewTask() {
   return (
-    <button className="hidden md:inline-flex items-center px-4 py-2 
-                       bg-indigo-600 dark:bg-indigo-500 
-                       text-white dark:text-white 
-                       text-sm font-medium rounded 
-                       hover:bg-indigo-700 dark:hover:bg-indigo-600 
-                       transition cursor-pointer">
+    <Link
+      to="/nova-tarefa"
+      className="hidden md:inline-flex items-center px-4 py-2 
+                 bg-indigo-600 dark:bg-indigo-500 
+                 text-white dark:text-white 
+                 text-sm font-medium rounded 
+                 hover:bg-indigo-700 dark:hover:bg-indigo-600 
+                 transition cursor-pointer"
+    >
       + Nova Tarefa
-    </button>
+    </Link>
   );
 }
