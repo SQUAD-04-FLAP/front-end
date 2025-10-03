cade0079aa2c230bd5170d1b0c94ac99fb5c5632
Ryan Nardelli <ryannardelli@gmail.com>
2025-10-03 11:20:37 -0300

    removendo comentarios

--- src/components/ContainerLinks/index.jsx
+++ src/components/ContainerLinks/index.jsx
@@ -1,24 +1,3 @@
-// export function ContainerLinks({ 
-//   children, 
-//   bgColor = "bg-gray-100 dark:bg-gray-900", 
-//   textColor = "text-gray-800 dark:text-gray-100"
-// }) {
-//   return (
-//     <div
-//       className={`
-//         flex items-center gap-2 px-4 py-3 transition cursor-pointer 
-//         hover:bg-gray-200 dark:hover:bg-gray-800 
-//         hover:text-gray-900 dark:hover:text-gray-200 
-//         group 
-//         ${bgColor} 
-//         ${textColor}
-//       `}
-//     >
-//       {children}
-//     </div>
-//   );
-// }
-
 export function ContainerLinks({ 
   children, 
   bgColor = "bg-gray-100 dark:bg-gray-900", 
