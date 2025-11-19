ebc84ec30ba769a476b774ec40587a73349aaf38
Ryan Nardelli <ryannardelli@gmail.com>
2025-11-19 00:58:24 -0300

    style(user-management): Adciionado modal para registro de usuário

--- src/pages/Usuarios/index.jsx
+++ src/pages/Usuarios/index.jsx
@@ -1,9 +1,10 @@
+import { ButtonRegisterNewUser } from '../../components/ButtonRegisterNewUser';
 import { TableUsers } from '../../components/TableUsers';
 
 export function Usuarios() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <main className="flex-1 p-10">
+      <main className="flex-1 p-8">
         <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100 mb-1">
           Gerenciamento de Usuários
         </h1>
@@ -12,12 +13,7 @@ export function Usuarios() {
         </p>
 
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 mb-4">
-          <button
-            className="flex items-center gap-2 px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition"
-          >
-            + Cadastrar novo usuário
-          </button>
-          
+          <ButtonRegisterNewUser />
         </div>
       </main>
       <TableUsers />
